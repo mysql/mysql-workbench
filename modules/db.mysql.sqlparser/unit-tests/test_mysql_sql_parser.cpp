762724008b6afefbd6910c1f5c827a7a8fbc0f05
Mike Lischke <mike.lischke@oracle.com>
2014-06-05 12:58:09 +0200

    - Parser module: added handling for completely messed up routine sql in groups. Make sure we still add a routine and the original sql, so the user doesn't lose his work. - Routine group editor: added helper method to help testing it's parsing functionality. - TUT: parser tests: some clean up. - TUT: routine group tests: updated to new group parsing.
    [TESTS]
      Ran the tests locally.

--- modules/db.mysql.sqlparser/unit-tests/test_mysql_sql_parser.cpp
+++ modules/db.mysql.sqlparser/unit-tests/test_mysql_sql_parser.cpp
@@ -44,7 +44,7 @@ END_TEST_DATA_CLASS
 TEST_MODULE(mysql_sql_parser, "SQL Parser (MySQL)");
 
 
-TEST_FUNCTION(1)
+TEST_FUNCTION(10)
 {
   wbt.create_new_document();
   GRT *grt= wbt.grt;
@@ -65,119 +65,120 @@ void Test_object_base<mysql_sql_parser>::test_import_sql(int test_no, const char
 {
   static const char* TEST_DATA_DIR = "data/modules_grt/wb_mysql_import/sql/";
 
-  /* set filenames & messages based on test no. */
-  std::ostringstream oss;
-  oss << test_no;
-  std::string test_message =                "SQL (" + oss.str() + ")";
-  std::string test_sql_filename =           TEST_DATA_DIR + oss.str() + ".sql";
-  std::string test_catalog_state_filename = TEST_DATA_DIR + oss.str() + ".xml";
-  std::string res_catalog_state_filename =  TEST_DATA_DIR + oss.str() + "_res.xml";
+  // Set filenames & messages based on test number.
+  std::string number_string = base::to_string(test_no);
+  std::string test_message = "SQL (" + number_string + ")";
+  std::string test_sql_filename = TEST_DATA_DIR + number_string + ".sql";
+  std::string test_catalog_state_filename = TEST_DATA_DIR + number_string + ".xml";
+  std::string res_catalog_state_filename = TEST_DATA_DIR + number_string + "_res.xml";
 
-  /* use initialized grt */
-  GRT* grt= rdbms.get_grt();
+  GRT* grt = rdbms.get_grt();
 
-  /* create & init new catalog */
+  // Create and init a new catalog.
   db_mysql_CatalogRef res_catalog(grt);
   res_catalog->version(rdbms->version());
   res_catalog->defaultCharacterSetName("utf8");
   res_catalog->defaultCollationName("utf8_general_ci");
   grt::replace_contents(res_catalog->simpleDatatypes(), rdbms->simpleDatatypes());
 
-  /* parse sql */
+  // Parse the sql.
   sql_facade->parseSqlScriptFileEx(res_catalog, test_sql_filename, options);
 
-  /* rename schema if asked */
+  // Rename the schema if asked.
   if (old_schema_name && new_schema_name)
     sql_facade->renameSchemaReferences(res_catalog, old_schema_name, new_schema_name);
 
-  /* serialization */
+  // Serialize the catalog to file (not necessary for this test, but for manual checks).
   grt->serialize(res_catalog, res_catalog_state_filename);
 
-  /* unserialization */
-  db_CatalogRef test_catalog= db_mysql_CatalogRef::cast_from(ValueRef(grt->unserialize(test_catalog_state_filename)));
+  // Unserialize the result so we can compare that with the generated catalog.
+  db_CatalogRef test_catalog = db_mysql_CatalogRef::cast_from(ValueRef(grt->unserialize(test_catalog_state_filename)));
 
-  /* comparison */
   grt_ensure_equals(test_message.c_str(), res_catalog, test_catalog);
+
+  //grt::replace_contents(res_catalog->characterSets(), rdbms->characterSets());
 }
 
+// Table
+TEST_FUNCTION(20)
+{
+  for (int i = 0; i < 18; ++i)
+    test_import_sql(i);
+}
 
-/* TABLE */
-TEST_FUNCTION(2) { test_import_sql(0); }
-TEST_FUNCTION(3) { test_import_sql(1); }
-TEST_FUNCTION(4) { test_import_sql(2); }
-TEST_FUNCTION(5) { test_import_sql(3); }
-TEST_FUNCTION(6) { test_import_sql(4); }
-TEST_FUNCTION(7) { test_import_sql(5); }
-TEST_FUNCTION(8) { test_import_sql(6); }
-TEST_FUNCTION(9) { test_import_sql(7); }
-TEST_FUNCTION(10) { test_import_sql(8); }
-TEST_FUNCTION(11) { test_import_sql(9); }
-TEST_FUNCTION(12) { test_import_sql(10); }
-TEST_FUNCTION(13) { test_import_sql(11); }
-TEST_FUNCTION(14) { test_import_sql(12); }
-TEST_FUNCTION(15) { test_import_sql(13); }
-TEST_FUNCTION(16) { test_import_sql(14); }
-TEST_FUNCTION(17) { test_import_sql(15); }
-TEST_FUNCTION(18) { test_import_sql(16); }
-TEST_FUNCTION(19) { test_import_sql(17); }
-TEST_FUNCTION(20) { test_import_sql(18); }
+// Index
+TEST_FUNCTION(30)
+{
+  test_import_sql(50);
+  test_import_sql(51);
+}
 
+// View
+TEST_FUNCTION(40)
+{
+  test_import_sql(100);
+  test_import_sql(101);
+}
 
-/* INDEX */
-TEST_FUNCTION(30) { test_import_sql(50); }
-TEST_FUNCTION(31) { test_import_sql(51); }
+// Routines
+TEST_FUNCTION(50)
+{
+  test_import_sql(150);
+  test_import_sql(151);
+}
 
+// Triggers
+TEST_FUNCTION(60)
+{
+  test_import_sql(200);
+}
 
-/* VIEW */
-TEST_FUNCTION(35) { test_import_sql(100); }
-TEST_FUNCTION(36) { test_import_sql(101); }
+// Events
+TEST_FUNCTION(70)
+{
+  /*
+  for (int i = 250; i < 254; ++i)
+    test_import_sql(i);
+  */
+}
 
+// Other language constructs.
+TEST_FUNCTION(80)
+{
+  // Logfile group + table space
+  test_import_sql(300);
 
-/* ROUTINE */
-TEST_FUNCTION(40) { test_import_sql(150); }
-TEST_FUNCTION(41) { test_import_sql(151); }
+  // Server link
+  test_import_sql(350);
 
+  // Alter statements
+  test_import_sql(400);
 
-/* TRIGGER */
-TEST_FUNCTION(45) { test_import_sql(200); }
+  // Drop statements
+  test_import_sql(450);
 
+  // Re-use of stub tables & columns
+  test_import_sql(600);
+}
 
-/* EVENT */
-//!TEST_FUNCTION(50) { test_import_sql(250); }
-//!TEST_FUNCTION(51) { test_import_sql(251); }
-//!TEST_FUNCTION(52) { test_import_sql(252); }
-//!TEST_FUNCTION(53) { test_import_sql(253); }
+// Real workd schemata (many objects) + other tasks.
+TEST_FUNCTION(90)
+{
+  // sakila-db: schema structures (except of triggers).
+  test_import_sql(700);
 
+  // sakila-db: inserts & triggers
+  test_import_sql(701);
+  
+  // sakila-db: mysqldump file
+  test_import_sql(702);
 
-/* LOGFILE GROUP, TABLESPACE */
-TEST_FUNCTION(55) { test_import_sql(300); }
+  // sakila-db: mysqldump file
+  test_import_sql(703, "sakila", "new_schema_name");
 
-
-/* SERVER LINK */
-TEST_FUNCTION(60) { test_import_sql(350); }
-
-
-/* ALTER STATEMENTS */
-TEST_FUNCTION(61) { test_import_sql(400); }
-
-
-/* DROP STATEMENTS */
-TEST_FUNCTION(62) { test_import_sql(450); }
-
-
-/* MISC */
-TEST_FUNCTION(65) { test_import_sql(600); } // re-use of stub tables & columns
-
-
-/* REAL-WORLD SCHEMATA (MANY OBJECTS) */
-TEST_FUNCTION(70) { test_import_sql(700); } // sakila-db: schema structures (except of triggers)
-TEST_FUNCTION(71) { test_import_sql(701); } // sakila-db: inserts & triggers
-TEST_FUNCTION(72) { test_import_sql(702); } // sakila-db: mysqldump file
-TEST_FUNCTION(73) { test_import_sql(703, "sakila", "new_schema_name"); } // sakila-db: mysqldump file
-
-
-/* SCHEMA RENAME */
-TEST_FUNCTION(80) { test_import_sql(900, "test", "new_schema_name"); }
+  // Schema rename.
+  test_import_sql(900, "test", "new_schema_name");
+}
 
 //--------------------------------------------------------------------------------------------------
 
@@ -248,101 +249,95 @@ void check_fwd_engineer(WBTester &wbt, std::string &modelfile, std::string &expe
   wbt.wb->close_document_finish();
 }
 
-TEST_FUNCTION(90)
+TEST_FUNCTION(100)
 {
-  //general test for forward engineer of sakilla database
+  // General test for forward engineer of sakila database.
   std::map<std::string, bool> opts;
   std::string modelfile = "data/forward_engineer/sakila.mwb";
   std::string expected_sql = "data/forward_engineer/sakila.expected.sql";
 
 
-  opts.insert(std::make_pair("GenerateDrops", 1));
-  opts.insert(std::make_pair("GenerateSchemaDrops", 1));
-  opts.insert(std::make_pair("SkipForeignKeys", 1));
-  opts.insert(std::make_pair("SkipFKIndexes", 1));
-  opts.insert(std::make_pair("GenerateWarnings", 1));
-  opts.insert(std::make_pair("GenerateCreateIndex", 1));
-  opts.insert(std::make_pair("NoUsersJustPrivileges", 0));
-  opts.insert(std::make_pair("NoViewPlaceholders", 0));
-  opts.insert(std::make_pair("GenerateInserts", 0));
-  opts.insert(std::make_pair("NoFKForInserts", 0));
-  opts.insert(std::make_pair("TriggersAfterInserts", 1));
-  opts.insert(std::make_pair("OmitSchemata", 0));
-  opts.insert(std::make_pair("GenerateUse", 1));
+  opts["GenerateDrops"] = true;
+  opts["GenerateSchemaDrops"] = true;
+  opts["SkipForeignKeys"] = true;
+  opts["SkipFKIndexes"] = true;
+  opts["GenerateWarnings"] = true;
+  opts["GenerateCreateIndex"] = true;
+  opts["NoUsersJustPrivileges"] = false;
+  opts["NoViewPlaceholders"] = false;
+  opts["GenerateInserts"] = false;
+  opts["NoFKForInserts"] = false;
+  opts["TriggersAfterInserts"] = true;
+  opts["OmitSchemata"] = false;
+  opts["GenerateUse"] = true;
 
-  opts.insert(std::make_pair("TablesAreSelected", 1));
-  opts.insert(std::make_pair("TriggersAreSelected", 1));
-  opts.insert(std::make_pair("RoutinesAreSelected", 1));
-  opts.insert(std::make_pair("ViewsAreSelected", 1));
-  opts.insert(std::make_pair("UsersAreSelected", 1));
+  opts["TablesAreSelected"] = true;
+  opts["TriggersAreSelected"] = true;
+  opts["RoutinesAreSelected"] = true;
+  opts["ViewsAreSelected"] = true;
+  opts["UsersAreSelected"] = true;
 
   check_fwd_engineer(wbt, modelfile, expected_sql, opts);
-
 }
 
-TEST_FUNCTION(91)
+TEST_FUNCTION(110)
 {
-  //foward engineer test of routine with ommitSchemata enabled
+  // Forward engineer test of routine with ommitSchemata enabled.
   std::map<std::string, bool> opts;
   std::string modelfile = "data/forward_engineer/ommit_schema_routine.mwb";
   std::string expected_sql = "data/forward_engineer/ommit_schema_routine.expected.sql";
 
+  opts["GenerateDrops"] = true;
+  opts["GenerateSchemaDrops"] = false;
+  opts["SkipForeignKeys"] = true;
+  opts["SkipFKIndexes"] = false;
+  opts["GenerateWarnings"] = false;
+  opts["GenerateCreateIndex"] = false;
+  opts["NoUsersJustPrivileges"] = false;
+  opts["NoViewPlaceholders"] = false;
+  opts["GenerateInserts"] = false;
+  opts["NoFKForInserts"] = false;
+  opts["TriggersAfterInserts"] = false;
+  opts["OmitSchemata"] = true;
+  opts["GenerateUse"] = false;
 
-  opts.insert(std::make_pair("GenerateDrops", true));
-  opts.insert(std::make_pair("GenerateSchemaDrops", false));
-  opts.insert(std::make_pair("SkipForeignKeys", true));
-  opts.insert(std::make_pair("SkipFKIndexes", false));
-  opts.insert(std::make_pair("GenerateWarnings", false));
-  opts.insert(std::make_pair("GenerateCreateIndex", false));
-  opts.insert(std::make_pair("NoUsersJustPrivileges", false));
-  opts.insert(std::make_pair("NoViewPlaceholders", false));
-  opts.insert(std::make_pair("GenerateInserts", false));
-  opts.insert(std::make_pair("NoFKForInserts", false));
-  opts.insert(std::make_pair("TriggersAfterInserts", false));
-  opts.insert(std::make_pair("OmitSchemata", true));
-  opts.insert(std::make_pair("GenerateUse", false));
-
-  opts.insert(std::make_pair("TablesAreSelected", true));
-  opts.insert(std::make_pair("TriggersAreSelected", false));
-  opts.insert(std::make_pair("RoutinesAreSelected", true));
-  opts.insert(std::make_pair("ViewsAreSelected", false));
-  opts.insert(std::make_pair("UsersAreSelected", true));
+  opts["TablesAreSelected"] = true;
+  opts["TriggersAreSelected"] = false;
+  opts["RoutinesAreSelected"] = true;
+  opts["ViewsAreSelected"] = false;
+  opts["UsersAreSelected"] = true;
 
   check_fwd_engineer(wbt, modelfile, expected_sql, opts);
-
 }
 
-TEST_FUNCTION(92)
+TEST_FUNCTION(120)
 {
-  //foward engineer test of routine with ommitSchemata enabled
+  // Forward engineer test of routine with ommitSchemata enabled
   std::map<std::string, bool> opts;
   std::string modelfile = "data/forward_engineer/schema_rename.mwb";
   std::string expected_sql = "data/forward_engineer/schema_rename.expected.sql";
 
+  opts["GenerateDrops"] = true;
+  opts["GenerateSchemaDrops"] = true;
+  opts["SkipForeignKeys"] = true;
+  opts["SkipFKIndexes"] = true;
+  opts["GenerateWarnings"] = true;
+  opts["GenerateCreateIndex"] = true;
+  opts["NoUsersJustPrivileges"] = true;
+  opts["NoViewPlaceholders"] = true;
+  opts["GenerateInserts"] = true;
+  opts["NoFKForInserts"] = true;
+  opts["TriggersAfterInserts"] = true;
+  opts["OmitSchemata"] = true;
+  opts["GenerateUse"] = true;
 
-  opts.insert(std::make_pair("GenerateDrops", 1));
-  opts.insert(std::make_pair("GenerateSchemaDrops", 1));
-  opts.insert(std::make_pair("SkipForeignKeys", 1));
-  opts.insert(std::make_pair("SkipFKIndexes", 1));
-  opts.insert(std::make_pair("GenerateWarnings", 1));
-  opts.insert(std::make_pair("GenerateCreateIndex", 1));
-  opts.insert(std::make_pair("NoUsersJustPrivileges", 1));
-  opts.insert(std::make_pair("NoViewPlaceholders", 1));
-  opts.insert(std::make_pair("GenerateInserts", 1));
-  opts.insert(std::make_pair("NoFKForInserts", 1));
-  opts.insert(std::make_pair("TriggersAfterInserts", 1));
-  opts.insert(std::make_pair("OmitSchemata", 1));
-  opts.insert(std::make_pair("GenerateUse", 1));
-
-  opts.insert(std::make_pair("TablesAreSelected", 1));
-  opts.insert(std::make_pair("TriggersAreSelected", 1));
-  opts.insert(std::make_pair("RoutinesAreSelected", 1));
-  opts.insert(std::make_pair("ViewsAreSelected", 1));
-  opts.insert(std::make_pair("UsersAreSelected", 1));
+  opts["TablesAreSelected"] = true;
+  opts["TriggersAreSelected"] = true;
+  opts["RoutinesAreSelected"] = true;
+  opts["ViewsAreSelected"] = true;
+  opts["UsersAreSelected"] = true;
 
   check_fwd_engineer(wbt, modelfile, expected_sql, opts);
-
 }
-
 
 END_TESTS
