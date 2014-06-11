c2d38b60d39bba8a3ada3a43301e92828d5097c8
Mike Lischke <mike.lischke@oracle.com>
2014-06-11 14:30:33 +0200

    - Win: modeling: properly unregister event callbacks and delete inner object when freeing note and stored note editors. - Win: renamed (stored)note editor wrappers files and classes. Created cpp files for previously header only files. - Fixes Bug #18718784 UNEXPECTED ERROR IN MYSQL WORKBENCH EDITING TEXT OBJECT
    [TESTS]
      - Followed the steps as in the issue report and checked the issue is gone.
      - Did additional test with multiple object editors, closing/reopening etc.

--- plugins/wb.model.editors/windows/wrapper/NoteEditorWrapper.h
+++ plugins/wb.model.editors/windows/wrapper/NoteEditorWrapper.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2012, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,5 +17,31 @@
  * 02110-1301  USA
  */
 
-#include "ImageEditorBE.h"
-#include "LayerEditorBE.h"
+#pragma once
+
+#include "wb_editor_note.h"
+#include "GrtTemplates.h"
+
+#pragma make_public(NoteEditorBE)
+
+namespace MySQL {
+namespace Grt {
+
+public ref class NoteEditorWrapper : public BaseEditorWrapper
+{
+protected:
+  NoteEditorWrapper(NoteEditorBE *inn);
+
+public:
+  NoteEditorWrapper(MySQL::Grt::GrtManager^ grtm, MySQL::Grt::GrtValue^ arglist);
+  ~NoteEditorWrapper();
+
+  NoteEditorBE *get_unmanaged_object();
+  void set_text(String ^text);
+  String^ get_text();
+  void set_name(String ^name);
+  String^ get_name();
+};
+
+} // namespace Grt
+} // namespace MySQL
