1ae9bc9b00fbc84f06d75bfe035960939b870220
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2014-06-27 10:28:37 -0700

    - Added support for creating a Connector/Python connection from a SQL Editor object (createCPyConnection)
    - Removed unused function in wb_admin_utils.py
    [TESTS]
    - tested creaton and use of connector/python connections from shell

--- backend/wbpublic/objimpl/db.query/db_query_Editor.h
+++ backend/wbpublic/objimpl/db.query/db_query_Editor.h
@@ -55,6 +55,8 @@ public:
 
   virtual db_query_ResultsetRef executeManagementQuery(const std::string &sql, bool log)= 0;
   virtual void executeManagementCommand(const std::string &sql, bool log)= 0;
+
+  virtual grt_PyObjectRef createCPyConnection()= 0;
 };
 
 
