2047d744f28721c40f54d6dd0a18008b5735651e
Rene Ramirez <j.rene.ramirez@oracle.com>
2014-06-30 14:28:21 -0500

    Fabric: Removing the Remote Management and System Profile tabs from the Connection Manager for fabric connections.
    [TESTS]
    - Ensures the tabs are not shown on fabric connections
    - Ensured the tabs are shown on other connection types.

--- frontend/common/server_instance_editor.h
+++ frontend/common/server_instance_editor.h
@@ -101,6 +101,7 @@ class MYSQLWBBACKEND_PUBLIC_FUNC ServerInstanceEditor : public mforms::Form
   mforms::TextEntry _sudo_prefix;
   
   grtui::DbConnectPanel *_connect_panel;
+  mforms::Box _remote_admin_box;
   
 //  mforms::Button _save_preset_button;
 //  mforms::Button _delete_preset_button;
