c1b447c936a1a59a8c90c4a660056da4ca6b3c4b
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2014-07-15 19:51:16 -0700

    SQLIDE: Fixed autosave configuration - wrong option name was used in prefs panel - closes Bug #19201716	 AUTO-SAVE HAPPENS EVERY 10 SECONDS REGARDLESS OF CONFIGURED SETTING [TESTS] - configure autosave and check breakpoint in autosave method

--- frontend/common/preferences_form.cpp
+++ frontend/common/preferences_form.cpp
@@ -796,7 +796,7 @@ mforms::View *PreferencesForm::create_sqlide_page()
 
       {
         static const char *auto_save_intervals= "disable:0,10 seconds:10,15 seconds:15,30 seconds:30,1 minute:60,5 minutes:300,10 minutes:600,20 minutes:1200";
-        mforms::Selector *sel = new_selector_option("workbench:AutoSaveScriptsInterval", auto_save_intervals, true);
+        mforms::Selector *sel = new_selector_option("workbench:AutoSaveSQLEditorInterval", auto_save_intervals, true);
 
         table->add_option(sel, _("Auto-save scripts interval:"),
                           _("Interval to perform auto-saving of all open script tabs.\nThe scripts will be restored from the last auto-saved version\nif Workbench unexpectedly quits."));
