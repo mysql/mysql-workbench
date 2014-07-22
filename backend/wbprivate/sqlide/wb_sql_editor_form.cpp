73d47bc8e17df1c4bc0111ef24958e66755e35cf
Alfredo Kojima <alfredo.kengi.kojima@oracle.com>
2014-07-22 14:08:17 -0700

    Removed attempt at fail safe apply of ALTER Object editor changes. The failback script was 99% of the time wrong and causes confusing error messages when it fails executing. Bug #13640795	 FAILBACK SCRIPT FOR ALTER TABLE EDITOR IS WRONG
    - removed deprecated files from msi
    [TESTS]
    - make bad change in alter table editor, apply, inspect that only the expected SQL is executed

--- backend/wbprivate/sqlide/wb_sql_editor_form.cpp
+++ backend/wbprivate/sqlide/wb_sql_editor_form.cpp
@@ -2391,6 +2391,7 @@ void SqlEditorForm::apply_object_alter_script(std::string &alter_script, bec::DB
   
   int max_query_size_to_log = _grtm->get_app_option_int("DbSqlEditor:MaxQuerySizeToHistory", 0);
   
+/* this doesn't really work
   std::list<std::string> failback_statements;
   if (obj_editor)
   {
@@ -2406,12 +2407,13 @@ void SqlEditorForm::apply_object_alter_script(std::string &alter_script, bec::DB
         failback_statements.push_back(sql);
       sql_splitter->splitSqlScript(original_object_ddl_script, failback_statements);
     }
-  }
+  }*/
   
   sql::SqlBatchExec sql_batch_exec;
   sql_batch_exec.stop_on_error(true);
-  sql_batch_exec.failback_statements(failback_statements);
-  
+//  if (!failback_statements.empty())
+//    sql_batch_exec.failback_statements(failback_statements);
+
   sql_batch_exec.error_cb(boost::ref(on_sql_script_run_error));
   sql_batch_exec.batch_exec_progress_cb(boost::ref(on_sql_script_run_progress));
   sql_batch_exec.batch_exec_stat_cb(boost::ref(on_sql_script_run_statistics));
