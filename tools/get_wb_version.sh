97312b4835f278b17ecead25dc908b013176aa73
Miguel Tadeu <miguel.tadeu@oracle.com>
2014-07-28 19:09:08 +0100

    Makefiles:   - Added workbench version variables   - Added shared object versions to .so names (somelib.so.x.y.z + symlink to .so) for better dependency checks during packaging   - Added link libraries for better dependency checks during packaging   - Added a script to retrieve WB version from wb_version.h   - Automated version from the script in cmake files Packaging: debian:   - Added debug symbol package generation   - Make the gen_deb_sources.py keep the original file permissions   - Describe the right path to the copyright file in debian/docs
    [TESTS]
    - Tested package generation localy
    - Build and run workbench on the libraries with the new names

---
+++ tools/get_wb_version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+while read line; do
+  line_array=($line)
+  
+  if [[ $line == *APP_MAJOR_NUMBER* ]]; then APP_MAJOR_NUMBER=${line_array[2]};
+  elif [[ $line == *APP_MINOR_NUMBER* ]]; then APP_MINOR_NUMBER=${line_array[2]};
+  elif [[ $line == *APP_RELEASE_NUMBER* ]]; then APP_RELEASE_NUMBER=${line_array[2]};
+  elif [[ $line == *APP_REVISION_NUMBER* ]]; then APP_REVISION_NUMBER=${line_array[2]};
+  elif [[ $line == *APP_BUILD_NUMBER* ]]; then APP_BUILD_NUMBER=${line_array[2]};
+  elif [[ $line == *APP_RELEASE_TYPE* ]]; then APP_RELEASE_TYPE=${line_array[2]};
+  elif [[ $line == *APP_LICENSE_TYPE* ]]; then APP_LICENSE_TYPE=${line_array[2]};
+  elif [[ $line == *APP_EDITION_NAME* ]]; then APP_EDITION_NAME=${line_array[2]};
+  fi
+done < ../backend/wbprivate/workbench/wb_version.h
+
+case "$1" in
+  "major") echo $APP_MAJOR_NUMBER;;
+  "minor") echo $APP_MINOR_NUMBER;;
+  "release") echo $APP_RELEASE_NUMBER;;
+  "revision") echo $APP_REVISION_NUMBER;;
+  "build") echo $APP_BUILD_NUMBER;;
+  "release_type") echo $APP_RELEASE_TYPE;;
+  "license") echo $APP_LICENSE_TYPE;;
+  "edition") echo $APP_EDITION_NAME;;
+  "full_version") echo "$APP_MAJOR_NUMBER.$APP_MINOR_NUMBER.$APP_RELEASE_NUMBER";;
+  *) echo "Invalid argument....";;
+esac
+
