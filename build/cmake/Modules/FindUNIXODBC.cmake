fe76b03c0268246a89eb868a7996c47a1ca32eab
Marcin Szalowicz <marcin.szalowicz@oracle.com>
2014-09-03 13:39:08 +0200

    - CMakeLists: add autodetection of iodbc vs unixodbc with option to force unixodbc - Cmake: FindIODBC/FindUnixodbc: fix it so it will not fail when iodbc/unixodbc will not be found [TESTS]  - build wb

--- build/cmake/Modules/FindUNIXODBC.cmake
+++ build/cmake/Modules/FindUNIXODBC.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -17,15 +17,17 @@
 
 find_path(UNIXODBC_INCLUDE_DIR unixodbc_conf.h)
 
-find_library(UNIXODBC_LIBRARY odbc)
+if (UNIXODBC_INCLUDE_DIR)
+  find_library(UNIXODBC_LIBRARY odbc)
 
-set(UNIXODBC_INCLUDE_DIRS ${UNIXODBC_INCLUDE_DIR} )
-set(UNIXODBC_LIBRARIES ${UNIXODBC_LIBRARY} )
+  set(UNIXODBC_INCLUDE_DIRS ${UNIXODBC_INCLUDE_DIR} )
+  set(UNIXODBC_LIBRARIES ${UNIXODBC_LIBRARY} )
 
-include(FindPackageHandleStandardArgs)
+  include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set UNIXODBC_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(unixODBC  DEFAULT_MSG
-                                  UNIXODBC_LIBRARY UNIXODBC_INCLUDE_DIR)
+  find_package_handle_standard_args(unixODBC  DEFAULT_MSG
+                                    UNIXODBC_LIBRARY UNIXODBC_INCLUDE_DIR)
 
-mark_as_advanced(UNIXODBC_INCLUDE_DIR UNIXODBC_LIBRARY)
+  mark_as_advanced(UNIXODBC_INCLUDE_DIR UNIXODBC_LIBRARY)
+endif (UNIXODBC_INCLUDE_DIR)
