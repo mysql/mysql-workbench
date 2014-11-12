975e2633bca8901de50ed34167edbbae8c53a484
Rene Ramirez <j.rene.ramirez@oracle.com>
2014-11-12 11:29:11 -0600

    - Fabric: Created wb.fabric.interface, a WB Module to be used as a connection interface to a MySQL Fabric Instance using the mysqlrpc protocol (Through libmysql).               - Using the client lib since connecting through the Connector/CPP was impossible due to the transactions done to fill metadata which are invalid on a Fabric node.               Refactored the existing fabric module to perform the interaction with the fabric instance through the new fabric interface module.
    Windows Fix for Bug #19915013: WORKBENCH DOES NOT WORK WITH FABRIC 1.5.2
    
    [TESTS]
    - Windows Only: Tested connecting and recreating managed fabric connections using a 1.5.3 Fabric Instance

---
+++ modules/wb.fabric.interface/stdafx.h
@@ -0,0 +1,34 @@
+/*
+* Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; version 2 of the
+* License.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301  USA
+*/
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <WinSock.h>
+
+
+
+// TODO: reference additional headers your program requires here
