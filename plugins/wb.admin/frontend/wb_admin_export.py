b4873cebea1eb696393ae0df3eddee18e0254eac
Milosz Bodzek <milosz.bodzek@oracle.com>
2015-01-05 17:19:12 +0100

    - fixed syntax error in wb_admin_export.py python file (patch sended by Christian Hesse <mail@eworm.de>)

--- plugins/wb.admin/frontend/wb_admin_export.py
+++ plugins/wb.admin/frontend/wb_admin_export.py
@@ -1709,10 +1709,10 @@ class WbAdminExportTab(WbAdminSchemaListTab):
                         # description, object_count, pipe_factory, extra_args, objects
                         args = []
                         if not dump_triggers:
-                            args.append['--skip-triggers']
+                            args.append('--skip-triggers')
                             
                         if skip_table_structure:
-                            args.append['--no-create-info']
+                            args.append('--no-create-info')
 
                         if skip_data:
                             task = self.TableDumpNoData(schema,table, args, lambda schema=schema,table=table:self.dump_to_folder(schema, table))
