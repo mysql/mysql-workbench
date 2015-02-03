5ee911a5963bba44b91c5779294fd281ec9acf5d
Marcin Szalowicz <marcin.szalowicz@oracle.com>
2015-02-03 14:32:36 +0100

    -  linux: mforms: form: change can_close into can_delete and fix the function call
    review by Mike

--- library/forms/gtk/lf_form.h
+++ library/forms/gtk/lf_form.h
@@ -45,7 +45,7 @@ class FormImpl : public ViewImpl
   void accept_clicked(bool *status, const bool is_run);
   void cancel_clicked(bool *status, const bool is_run);
   bool on_widget_delete_event(GdkEventAny* event, Button *cancel);
-  bool can_close_widget(GdkEventAny* event);
+  bool can_delete_widget(GdkEventAny* event);
   static void show_modal(::mforms::Form *self, ::mforms::Button *accept, ::mforms::Button *cancel);
   static void end_modal(::mforms::Form *self, bool result);
   bool on_key_release(GdkEventKey* event, bool *status, const bool is_run, ::mforms::Button *accept, ::mforms::Button *cancel);
