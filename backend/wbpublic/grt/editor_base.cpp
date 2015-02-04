563ea2b6bfb85c332f9ee198f100b7f8b4d099dd
Mike Lischke <mike.lischke@oracle.com>
2015-02-04 20:53:53 +0100

    MW-691 [SQLED] Assertion failure when closing a just created schema tab Object editors: don't create an sql editor if not necessary, as this happens on demand and can lead to creating a new editor on close of the object editor (when checking for the dirty state).
    Review by: lolek (rb: #7940)

--- backend/wbpublic/grt/editor_base.cpp
+++ backend/wbpublic/grt/editor_base.cpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2007, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2015, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -178,6 +178,9 @@ void BaseEditor::run_from_grt(const boost::function<void()> &slot)
 
 bool BaseEditor::is_editor_dirty()
 {
+  if (!has_editor())
+    return false;
+
   MySQLEditor::Ref editor = get_sql_editor();
   if (editor)
   {
