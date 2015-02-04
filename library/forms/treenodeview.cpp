43123f6938b36f96ec14050ae3fb2d10f0a7e940
Mike Lischke <mike.lischke@oracle.com>
2015-02-04 17:56:08 +0100

    Mac: fixes for auto completion popup - Mac: fixed black-on-dark blue text (selection) - backend: removed some more placeholders for which we don't show code completion. - Minor change in mforms::TreeNodeView.
    Review by: lolek (rb: #7927)

--- library/forms/treenodeview.cpp
+++ library/forms/treenodeview.cpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2012, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -132,6 +132,8 @@ TreeNodeRef TreeNode::find_child_with_tag(const std::string &tag)
   }
   return TreeNodeRef();
 }
+
+//--------------------------------------------------------------------------------------------------
 
 void TreeNode::toggle()
 {
