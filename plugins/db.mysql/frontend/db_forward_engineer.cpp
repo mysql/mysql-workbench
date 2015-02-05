eb0e6434f1030d42ceb7b9cd8f528398976a69ed
Milosz Bodzek <milosz.bodzek@oracle.com>
2015-02-05 14:26:54 +0100

    - fixed window size
    - close bug #20091734 - FORWARD ENGINEER WIZARD HIDDEN BUTTONS
    
    reviewed by Lolek (rb: n/a)

--- plugins/db.mysql/frontend/db_forward_engineer.cpp
+++ plugins/db.mysql/frontend/db_forward_engineer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -602,6 +602,7 @@ WbPluginDbExport::WbPluginDbExport(grt::Module *module)
   add_page(mforms::manage(_progress_page));
 
   set_title(_("Forward Engineer to Database"));
+  set_size(-1, 700);
 }
 
 
