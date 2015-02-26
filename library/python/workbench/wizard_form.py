21fa2c9d5ddb3ddc63c92fd3c10ad1c43ac89b7c
Miguel Tadeu <miguel.tadeu@oracle.com>
2015-02-26 16:15:03 +0000

    library:python:Add new functionalities to the wizard base
        Standard behavior for page skipping
        Set behavior for last page (finish button and exit)
    
    reviewed by: mikel

--- library/python/workbench/wizard_form.py
+++ library/python/workbench/wizard_form.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2014, 2015 Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -43,8 +43,11 @@ class WizardForm(mforms.Form):
 
 
     def add_page(self, page):
-        if not self.pages:
+        if self.pages:
+            self.pages[len(self.pages) - 1].set_last_page(False)
+        else:
             page.back_button.set_enabled(False)
+        page.set_last_page(True)
         self.pages.append(page)
         self.tabview.add_page(page, "")
 
@@ -53,17 +56,27 @@ class WizardForm(mforms.Form):
         current_page = self.tabview.get_active_tab()
         if not self.pages[current_page].validate():
             return
-        if current_page < len(self.pages)-1:
-            self.tabview.set_active_tab(current_page + 1)
-            self.pages[current_page + 1].page_activated(True)
+        if current_page == len(self.pages) - 1:
+            self.finish()
+        for index in range(current_page + 1, len(self.pages)):
+            if self.pages[index].should_skip():
+                continue
+            self.tabview.set_active_tab(index)
+            self.pages[index].page_activated(True)
+            break
 
 
     def go_previous_page(self):
         current_page = self.tabview.get_active_tab()
-        if current_page > 0:
-            self.tabview.set_active_tab(current_page-1)
-            self.pages[current_page].page_activated(False)
+        for index in range(current_page - 1, -1, -1):
+            if self.pages[index].should_skip():
+                continue
+            self.tabview.set_active_tab(index)
+            self.pages[index].page_activated(False)
+            break
 
+    def finish(self):
+        self.close()
 
     def cancel(self):
         self.end_modal(False)
