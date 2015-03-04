c6d627444ce2f41e263fd2d4e34fd095c2fc6e23
Mike Lischke <mike.lischke@oracle.com>
2015-03-04 17:06:25 +0100

    set version to 6.3.2

--- frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
+++ frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using MySQL.GUI.Workbench;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.3.1.0")]
-[assembly: AssemblyFileVersion("6.3.1.0")]
+[assembly: AssemblyVersion("6.3.2.0")]
+[assembly: AssemblyFileVersion("6.3.2.0")]
