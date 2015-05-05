be87bc05b53a7f2c67dd816ce2f061e2ec6506bd
Marcin Marek <marcin.marek@oracle.com>
2015-05-05 11:38:17 +0200

    [MW-975] - Object reference error in roles setup for user Fixed memory leaks
    Review by lolek [rev id: 8799]

--- backend/windows/wbpublic.wr/src/RoleTreeBE.cpp
+++ backend/windows/wbpublic.wr/src/RoleTreeBE.cpp
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2008, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -23,3 +23,81 @@
 #include "GrtManager.h"
 
 #include "RoleTreeBE.h"
+
+using namespace MySQL::Grt;
+using namespace MySQL::Grt::Db;
+
+//--------------------------------------------------------------------------------------------------
+
+RoleTreeBE::RoleTreeBE(::bec::RoleTreeBE *inn) 
+  : MySQL::Grt::TreeModelWrapper(inn), free_inner(false)
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+
+RoleTreeBE::RoleTreeBE(GrtValue^ catalog)
+  : MySQL::Grt::TreeModelWrapper(
+    new ::bec::RoleTreeBE(db_CatalogRef::cast_from(catalog->get_unmanaged_object()))), 
+    free_inner(true)
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+
+RoleTreeBE::~RoleTreeBE()
+{
+  if (free_inner)
+    delete inner;
+}
+
+//--------------------------------------------------------------------------------------------------
+
+::bec::RoleTreeBE* RoleTreeBE::get_unmanaged_object()
+{
+  return static_cast<::bec::RoleTreeBE *>(inner);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+MySQL::Grt::GrtValue^ RoleTreeBE::get_role_with_id(MySQL::Grt::NodeIdWrapper^ node)
+{
+  return gcnew MySQL::Grt::GrtValue(get_unmanaged_object()->get_role_with_id(*node->get_unmanaged_object()));
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void RoleTreeBE::erase_node(MySQL::Grt::NodeIdWrapper^ node)
+{
+  get_unmanaged_object()->erase_node(*node->get_unmanaged_object());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void RoleTreeBE::insert_node_before(MySQL::Grt::NodeIdWrapper^ before, MySQL::Grt::NodeIdWrapper^ node)
+{
+  get_unmanaged_object()->insert_node_before(*before->get_unmanaged_object(), *node->get_unmanaged_object());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void RoleTreeBE::insert_node_after(MySQL::Grt::NodeIdWrapper^ after, MySQL::Grt::NodeIdWrapper^ node)
+{
+  get_unmanaged_object()->insert_node_after(*after->get_unmanaged_object(), *node->get_unmanaged_object());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void RoleTreeBE::append_child(MySQL::Grt::NodeIdWrapper^ parent, MySQL::Grt::NodeIdWrapper^ child)
+{
+  get_unmanaged_object()->append_child(*parent->get_unmanaged_object(), *child->get_unmanaged_object());
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void RoleTreeBE::move_to_top_level(MySQL::Grt::NodeIdWrapper^ node)
+{
+  get_unmanaged_object()->move_to_top_level(*node->get_unmanaged_object());
+}
+
+//--------------------------------------------------------------------------------------------------
