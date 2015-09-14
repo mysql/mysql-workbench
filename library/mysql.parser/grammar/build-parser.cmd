120f58d69853d915ce7e3441fb74e4fbe2375401
Mike Lischke <mike.lischke@oracle.com>
2015-09-14 12:25:11 +0200

    Win: changed build script and custom build steps in the parsers project for generating full + simple MySQL parser and the ECMA parser files.

--- library/mysql.parser/grammar/build-parser.cmd
+++ library/mysql.parser/grammar/build-parser.cmd
@@ -5,6 +5,8 @@ if [%1] == [] goto Usage
 IF %1 == mysql (
   echo Selected MySQL parser
   java -Xmx1024m -jar ..\..\..\..\mysql-win-res\bin\antlr-3.4-complete.jar  -make MySQL.g -o .. -Xmaxswitchcaselabels 30
+) ELSE IF %1 == mysqls (
+  echo Selected simple MySQL parser
   java -Xmx1024m -jar ..\..\..\..\mysql-win-res\bin\antlr-3.4-complete.jar  -make MySQLSimpleParser.g -o .. -Xmaxswitchcaselabels 30
 ) ELSE IF %1 == python (
   echo Selected Python parser
@@ -17,6 +19,6 @@ IF %1 == mysql (
 goto EndOfScript
 
 :Usage
-echo "Usage: $0 [mysql|python|js]"
+echo "Usage: $0 [mysql|mysqls|python|js]"
 
 :EndOfScript
