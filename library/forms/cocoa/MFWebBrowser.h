025637875790735ee99201f83ebfb444a608f1eb
Mike Lischke <mike.lischke@oracle.com>
2015-09-19 15:37:47 +0200

    Mac: small corrections to make WB compile with XCode 7.

--- library/forms/cocoa/MFWebBrowser.h
+++ library/forms/cocoa/MFWebBrowser.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2010, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -24,7 +24,7 @@
 #import "mforms/WebBrowser.h"
 
 
-@interface MFWebBrowserImpl : NSView {
+@interface MFWebBrowserImpl : NSView <WebFrameLoadDelegate, WebUIDelegate, WebPolicyDelegate, WebResourceLoadDelegate> {
   mforms::WebBrowser* mOwner;
   WebView* mBrowser;
 }
