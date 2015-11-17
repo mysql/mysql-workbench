9995af4998c1f065ca26ec8198bb9dcc88baa3a1
Miguel Tadeu <miguel.tadeu@oracle.com>
2015-11-17 18:01:28 +0000

    build:cmake: Fixed CTemplate script
    reviewed by: lolek

--- build/cmake/Modules/FindCTemplate.cmake
+++ build/cmake/Modules/FindCTemplate.cmake
@@ -36,8 +36,24 @@ find_path(CTemplate_INCLUDE_DIR template_annotator.h
 
 
 set(CTemplate_NAMES ctemplate)
+
+
 if(CTemplate_LIBRARIES)
-  find_library(CTemplate_LIBRARY NAMES ${CTemplate_NAMES} HINTS ${CTemplate_LIBRARIES})
+  # Converto to a list of library argments
+  string(REPLACE " " ";" CTemplate_LIB_ARGS ${CTemplate_LIBRARIES})
+
+  # Parse the list in order to find the library path
+  foreach(CTemplate_LIB_ARG ${CTemplate_LIB_ARGS})
+    string(REPLACE "-L" "" CTemplate_LIB_ARG_CLEAR ${CTemplate_LIB_ARG})
+    if(NOT ${CTemplate_LIB_ARG_CLEAR} STREQUAL ${CTemplate_LIB_ARG})
+      set(CTemplate_SUPPLIED_LIB_DIR ${CTemplate_LIB_ARG_CLEAR})
+    endif()
+  endforeach(CTemplate_LIB_ARG)
+  find_library(CTemplate_LIBRARY NAMES ${CTemplate_NAMES} HINTS ${CTemplate_SUPPLIED_LIB_DIR})
+  
+  unset(CTemplate_LIB_ARG_CLEAR)
+  unset(CTemplate_LIB_ARG)
+  unset(CTemplate_LIB_ARGS)
 else()
   find_library(CTemplate_LIBRARY NAMES ${CTemplate_NAMES})
 endif()
@@ -48,9 +64,13 @@ get_filename_component(CTemplate_LIB_BASE_DIRECTORY ${CTemplate_LIB_DIRECTORY} D
 
 set(CTemplate_BIN_DIR "${CTemplate_LIB_BASE_DIRECTORY}/bin")
 
+# Allow to call find_program twice in order to find wothout the default paths and then with them.
+# If it finds on the first call, it will use the cached one.
 find_program(CTemplate_VARNAMES NAMES make_tpl_varnames_h ctemplate-make_tpl_varnames_h
-                                PATHS CTemplate_BIN_DIR
+                                PATHS ${CTemplate_BIN_DIR}
+                                NO_DEFAULT_PATH
             )
+find_program(CTemplate_VARNAMES NAMES make_tpl_varnames_h ctemplate-make_tpl_varnames_h)
 
 # Get CTemplate version
 execute_process(COMMAND ${CTemplate_VARNAMES} --version
