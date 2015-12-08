f44493e1eac06fb019e6ef520182a0b2366757e9
Mike Lischke <mike.lischke@oracle.com>
2015-12-08 13:45:16 +0100

    grt: separated repr() function into 2 (for debug + normal work)
    Some GRT objects (namely ValueRef + descendants) had a repr() function to turn their content into a string. This is regularly used as a debug help + for logging. However, it is also used for normal code execution, which made it impossible to improve output for debugging (e.g. adding formatting). Hence this function has been split into description() and toString() (the first for debugging, the latter for normal code usage).
    
    Review by: lolek (rb #11259)

--- backend/wbpublic/grtui/grtdb_connect_panel.cpp
+++ backend/wbpublic/grtui/grtdb_connect_panel.cpp
@@ -637,7 +637,7 @@ bool DbConnectPanel::test_connection()
     std::string ssl_cipher;
 
     message.append("Host: " + connectionProperties->parameterValues().get_string("hostName") + "\n");
-    message.append("Port: " + grt::IntegerRef(connectionProperties->parameterValues().get_int("port")).repr() + "\n");
+    message.append("Port: " + grt::IntegerRef(connectionProperties->parameterValues().get_int("port")).toString() + "\n");
     message.append("User: " + connectionProperties->parameterValues().get_string("userName") + "\n");
 
     if ( connectionProperties->driver()->name() == "MySQLFabric")
@@ -971,8 +971,8 @@ void DbConnectPanel::set_keychain_password(DbDriverParam *param, bool clear)
   }
   for (grt::DictRef::const_iterator iter = paramValues.begin(); iter != paramValues.end(); ++iter)
   {
-    storage_key = bec::replace_string(storage_key, "%"+iter->first+"%", iter->second.repr());
-    username = bec::replace_string(username, "%"+iter->first+"%", iter->second.repr());
+    storage_key = bec::replace_string(storage_key, "%" + iter->first + "%", iter->second.toString());
+    username = bec::replace_string(username, "%" + iter->first + "%", iter->second.toString());
   }
 
   if (username.empty())
@@ -1127,7 +1127,7 @@ void DbConnectPanel::create_control(::DbDriverParam *driver_param, const ::Contr
 
       // value
       {
-        grt::StringRef value= driver_param->get_value_repr();
+        grt::StringRef value = driver_param->get_value_repr();
         if (value.is_valid())
           ctrl->set_active(*value != "" && *value != "0" && *value != "NULL");
       }
