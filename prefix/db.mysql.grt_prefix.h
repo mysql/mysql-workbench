958d3703a496f6885b7f48b0ccc226ebbb7b9fe2
Mike Lischke <mike.lischke@oracle.com>
2016-01-12 14:40:19 +0100

    Fixes needed after the previous merge, mostly related to precompiled headers.

--- prefix/db.mysql.grt_prefix.h
+++ prefix/db.mysql.grt_prefix.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -25,7 +25,16 @@
 
 #ifdef __cplusplus
 
+#include <string>
+#include <sstream>
+#include <vector>
+#include <list>
+
 #include <glib.h>
 #include <glib/gstdio.h>
+
+#include <boost/optional.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/shared_ptr.hpp>
 
 #endif
