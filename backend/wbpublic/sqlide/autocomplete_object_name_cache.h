84afd6cee9e1dc0df23d19649425e97c5fc8e374
Mike Lischke <mike.lischke@oracle.com>
2016-02-01 11:13:18 +0100

    Implemented support for collations and charsets in code completion (and object name cache)
    Review by: lolek (rb #11668)

--- backend/wbpublic/sqlide/autocomplete_object_name_cache.h
+++ backend/wbpublic/sqlide/autocomplete_object_name_cache.h
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -63,6 +63,9 @@ public:
   std::vector<std::string> get_matching_logfile_groups(const std::string &prefix = ""); // Only useful for NDB cluster.
   std::vector<std::string> get_matching_tablespaces(const std::string &prefix = ""); // Only useful for NDB cluster.
 
+  std::vector<std::string> get_matching_charsets(const std::string &prefix = "");
+  std::vector<std::string> get_matching_collations(const std::string &prefix = "");
+
   // Data refresh functions. To be called from outside when data objects are created or destroyed.
   void refresh_schema_list();
   bool refresh_schema_cache_if_needed(const std::string &schema);
@@ -72,7 +75,7 @@ public:
   void refresh_triggers(const std::string &schema, const std::string &table);
   void refresh_udfs();
   void refresh_tablespaces();    // Logfile groups and tablespaces are unqualified,
-  void refresh_logfile_groups(); // event though they belong to a specific table.
+  void refresh_logfile_groups(); // even though they belong to a specific table.
 
   // Update functions that can also be called from outside.
   void update_schemas(const std::vector<std::string> &schemas);
@@ -106,6 +109,8 @@ private:
       RefreshEngines,
       RefreshLogfileGroups,
       RefreshTableSpaces,
+      RefreshCharsets,
+      RefreshCollations
     } type;
     std::string schema_name;
     std::string table_name;
@@ -142,6 +147,8 @@ private:
   void refresh_triggers_w(const std::string &schema, const std::string &table);
 
   void refresh_udfs_w();
+  void refreshCharsets_w();
+  void refreshCollations_w();
   void refresh_variables_w();
   void refresh_engines_w();
   void refresh_logfile_groups_w();
