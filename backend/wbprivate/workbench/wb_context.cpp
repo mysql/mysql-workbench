16a319899ddb1f012b05d79af8faf31c50e85175
Marcin Szalowicz <marcin.szalowicz@oracle.com>
2016-02-02 15:41:45 +0100

    configdir option fix OSX and Win build,
    review by Mike

--- backend/wbprivate/workbench/wb_context.cpp
+++ backend/wbprivate/workbench/wb_context.cpp
@@ -467,7 +467,7 @@ bool WBOptions::parse_args(char **argv, int argc, int *retval)
     }
     else if (check_arg_with_value(argv, i, "configdir", argval))
     {
-      if (argval == NULL || base::file_exists(argval))
+      if (argval == NULL)
       {
         printf("Invalid path specified as the configuration folder.\n");
         return false;
