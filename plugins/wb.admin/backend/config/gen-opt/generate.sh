a6bb850d59b92eb2be1862d80ec1ab2478b3843e
Miguel Tadeu <miguel.tadeu@oracle.com>
2016-02-24 10:16:39 +0000

    Setup more groups for the system and status variables     - Updated with the new docs file     - Editor dialog is now modal     - Canceling while selecting the directory now works     - Updated the instruction in the README file     - Disable the "Add Group" label/button when there is no selection available     - generate.sh script will now copy the proper files into the proper directories

--- plugins/wb.admin/backend/config/gen-opt/generate.sh
+++ plugins/wb.admin/backend/config/gen-opt/generate.sh
@@ -2,3 +2,9 @@ rm -f *.pyc
 python -B 1_mysqld2optlist.py
 python -B 2_genoptions_layout.py
 python -B 3_rawopts2opts.py
+
+if [ "$1" == "update" ]; then
+    echo "Copying files to the code directory..."
+    cp opts.py ../..
+    cp wb_admin_variable_list.py ../..
+fi
