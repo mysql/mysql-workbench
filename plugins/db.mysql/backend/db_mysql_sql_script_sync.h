610c363422cb0c15b1520d0949724aa206e7b5de
Marcin Marek <marcin.marek@oracle.com>
2016-03-01 10:01:45 +0100

    Fixed db_mysql_plugin_test after grt refactoring

--- plugins/db.mysql/backend/db_mysql_sql_script_sync.h
+++ plugins/db.mysql/backend/db_mysql_sql_script_sync.h
@@ -81,10 +81,10 @@ public:
   std::string get_sql_for_object(GrtNamedObjectRef obj);
 
   void set_options(grt::DictRef options) { _options = options; }
-  grt::DictRef get_options() const { return _options.is_valid() ? _options : grt::DictRef(); }
+  grt::DictRef get_options() const { return _options.is_valid() ? _options : grt::DictRef(true); }
 
   void set_db_options(grt::DictRef db_options) {_db_options = db_options;};
-  grt::DictRef get_db_options() const { return _db_options.is_valid() ? _db_options : grt::DictRef();}
+  grt::DictRef get_db_options() const { return _db_options.is_valid() ? _db_options : grt::DictRef(true);}
 
   grt::StringRef generate_alter(db_mysql_CatalogRef org_cat, db_mysql_CatalogRef org_cat_copy, db_mysql_CatalogRef mod_cat_copy);
 
