6a6876663bde9d3d48c4b71ceb8b9473bf93a8bd
Marcin Szalowicz <marcin.szalowicz@oracle.com>
2017-10-23 09:44:18 +0200

    Add ubuntu 17.10 as supported platform

--- build/gen_deb_sources.py
+++ build/gen_deb_sources.py
@@ -5,6 +5,7 @@
 
 
 output_distros = [
+    ("artful", "ubuntu17.10", "1710", ""),
     ("zesty", "ubuntu17.04", "1704", ""),
     ("yakkety", "ubuntu16.10", "1610", ""),
     ("xenial" ,"ubuntu16.04" ,"1604"  ,""),
