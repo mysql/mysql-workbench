121695e3535c26c7b62bf3c2a5e3d04bf3854ccf
Milosz Bodzek <milosz.bodzek@oracle.com>
2017-10-25 09:46:08 +0200

    added support for macOS 10.13 High Sierra

--- library/base/util_functions.cpp
+++ library/base/util_functions.cpp
@@ -369,6 +369,8 @@ std::string get_local_os_name() {
   *dot_position = 0;
   int version = base::atoi<int>(info.release, 0);
   switch (version) {
+    case 17:
+      return std::string("macOS 10.13.x High Sierra ") + info.machine;
     case 16:
       return std::string("macOS 10.12.x Sierra ") + info.machine;
     case 15:
