6416b309fd166fc029f00e574f456abe4de96458
Mike Lischke <mike.lischke@oracle.com>
2018-03-09 16:17:07 +0100

    Set version to 8.0.11.

--- frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
+++ frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using MySQL.GUI.Workbench;
 //      Minor Version 
 //      Release Number
 //
-[assembly: AssemblyVersion("8.0.5")]
-[assembly: AssemblyFileVersion("8.0.5")]
+[assembly: AssemblyVersion("8.0.11")]
+[assembly: AssemblyFileVersion("8.0.11")]
