92f7127a2e42754f624ce511a26e3ce5999af681
Mike Lischke <mike.lischke@oracle.com>
2018-05-04 11:09:24 +0200

    Updated LOC count script.
    The script was separately counting C files as ANTLR3 files, which is no longer the case (and wasn't entirely true anyway).
    Additionally, grammars and Javascript files are taken in as well now.

--- tools/count_loc.sh
+++ tools/count_loc.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-dirs="backend frontend  generated/grts library modules plugins prefix res"
-exts="c cpp cxx mm m py cs h"
+dirs="backend frontend  generated/grts internal library modules plugins prefix res"
+exts="c cpp cxx mm m py cs h js g4"
 
 headers=0
 total_files=0
 total=0
-parser_total=0
 for ext in $exts; do
     files=`find $dirs -name \*.$ext|grep -v unit-test|wc -l`
     count=$(wc -l /dev/null `find $dirs -name \*.$ext|grep -v unit-test`|tail -1|awk '{print $1}')
@@ -14,15 +13,11 @@ for ext in $exts; do
         headers=$files
     fi
 
-    if test $ext = c; then
-        ext="$ext (antlr parser)"
-        parser_total=$count
-    fi
     echo "$ext: $count loc  $files files"
     total=$(($total + $count))
     total_files=$(($total_files + $files))
 done
-echo "Total: $total ($(($total - $parser_total)) without parser)"
+echo "Total: $total"
 echo "Total Files: $total_files ($(($total_files - $headers)) without headers)"
 
 
