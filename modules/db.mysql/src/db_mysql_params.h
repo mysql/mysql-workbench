b5ef0d7041eabf89ed2cdc3380c5c296c3a634c1
Mike Lischke <mike.lischke@oracle.com>
2019-09-26 11:11:08 +0200

    Bug #30338754 SOME DATABASE ENGINES SEEMS NOT TO BE REALLY WORKING
    Signed-off-by: Mike Lischke <mike.lischke@oracle.com>

--- modules/db.mysql/src/db_mysql_params.h
+++ modules/db.mysql/src/db_mysql_params.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2019, Oracle and/or its affiliates. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2.0,
@@ -51,29 +51,5 @@ namespace dbmysql {
     return res;
   }
 
-  enum EngineId {
-    eetMySAM = 0,
-    eetInnoDB,
-    eetFalcon,
-    eetMerge,
-    eetMemory,
-    eetExample,
-    eetFederated,
-    eetArchive,
-    eetCsv,
-    eetBlackhole,
-    eetOTHER
-  };
-
-  EngineId MYSQLMODULEDBMYSQL_PUBLIC_FUNC engine_id_by_name(const char* name);
-
-  std::string MYSQLMODULEDBMYSQL_PUBLIC_FUNC engine_name_by_id(EngineId id);
-
-  db_mysql_StorageEngineRef MYSQLMODULEDBMYSQL_PUBLIC_FUNC engine_by_name(const char* name);
-
-  db_mysql_StorageEngineRef MYSQLMODULEDBMYSQL_PUBLIC_FUNC engine_by_id(EngineId id);
-
   grt::ListRef<db_mysql_StorageEngine> MYSQLMODULEDBMYSQL_PUBLIC_FUNC get_known_engines();
-
-  bool MYSQLMODULEDBMYSQL_PUBLIC_FUNC check_valid_characters(const char* str);
 }
