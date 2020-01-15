6b14f0ae382250ce3be9b8e58976ee45be2963c1
Miguel Tadeu <miguel.tadeu@oracle.com>
2020-01-15 16:07:05 +0000

    Python3 doesn't use the 'new' package

--- plugins/migration/frontend/migration_ui_style.py
+++ plugins/migration/frontend/migration_ui_style.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2007, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2007, 2020, Oracle and/or its affiliates. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License, version 2.0,
@@ -21,7 +21,6 @@
 
 import mforms
 import sys
-import new
 
 #-------------------------------------------------------------------------------
 def newHeaderLabel(text):
