a6f92de89e86cff1b8a332cd6ea877596042eaf1
Milosz Bodzek <milosz.bodzek@oracle.com>
2020-07-09 13:45:46 +0200

    Fixed python 3.7 build

--- library/grt/src/python_grtobject.cpp
+++ library/grt/src/python_grtobject.cpp
@@ -177,8 +177,10 @@ static PyTypeObject PyGRTMethodObjectType = {
   0,  //  tp_version_tag
 
   0, //  tp_finalize
+#if PY_MINOR_VERSION > 7
   nullptr,  //  tp_vectorcall
   nullptr   //  tp_print
+#endif
 };
 
 //----------------------------------------------------------------------------------------------
