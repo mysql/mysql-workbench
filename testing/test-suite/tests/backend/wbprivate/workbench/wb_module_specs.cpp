ba17a3ac5ed4b649fefd484c8aac595e15a9a1a8
Miguel Tadeu <miguel.tadeu@oracle.com>
2020-10-01 10:22:12 +0100

    Added support for Fedora33 and removed Fedora30 and 31

--- testing/test-suite/tests/backend/wbprivate/workbench/wb_module_specs.cpp
+++ testing/test-suite/tests/backend/wbprivate/workbench/wb_module_specs.cpp
@@ -129,9 +129,10 @@ $describe("wb_module tests for Workbench") {
     $expect(isOsSupportedProxy("Fedora release 27 x86_64")).toBeFalse();
     $expect(isOsSupportedProxy("Fedora release 28 x86_64")).toBeFalse();
     $expect(isOsSupportedProxy("Fedora release 29 x86_64")).toBeFalse();
-    $expect(isOsSupportedProxy("Fedora release 30 x86_64")).toBeTrue();
-    $expect(isOsSupportedProxy("Fedora release 31 x86_64")).toBeTrue();
+    $expect(isOsSupportedProxy("Fedora release 30 x86_64")).toBeFalse();
+    $expect(isOsSupportedProxy("Fedora release 31 x86_64")).toBeFalse();
     $expect(isOsSupportedProxy("Fedora release 32 x86_64")).toBeTrue();
+    $expect(isOsSupportedProxy("Fedora release 33 x86_64")).toBeTrue();
   });
 }
 }
