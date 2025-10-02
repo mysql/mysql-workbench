2ad12225a76a9e3c3704b49061545a7ef7ef0a1e
Marcin Marek <marcin.marek@oracle.com>
2025-10-02 13:25:11 +0200

    set version number to 8.0.44

--- backend/wbprivate/workbench/wb_version.h
+++ backend/wbprivate/workbench/wb_version.h
@@ -25,7 +25,7 @@
 
 #define APP_MAJOR_NUMBER 8
 #define APP_MINOR_NUMBER 0
-#define APP_RELEASE_NUMBER 43
+#define APP_RELEASE_NUMBER 44
 
 #define APP_BUILD_NUMBER 0
 #define APP_RELEASE_TYPE "commercial"
