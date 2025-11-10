13e9f0694a1ca1cc4b338e77da7a5a8320d2d014
Rene Ramirez <j.rene.ramirez@oracle.com>
2025-11-10 16:10:33 -0600

    Set version number to 8.0.45

--- backend/wbprivate/workbench/wb_version.h
+++ backend/wbprivate/workbench/wb_version.h
@@ -25,10 +25,10 @@
 
 #define APP_MAJOR_NUMBER 8
 #define APP_MINOR_NUMBER 0
-#define APP_RELEASE_NUMBER 44
+#define APP_RELEASE_NUMBER 45
 
 #define APP_BUILD_NUMBER 0
-#define APP_RELEASE_TYPE "commercial"
+#define APP_RELEASE_TYPE ""
 
 #define APP_LICENSE_TYPE "GPL"
 #define APP_EDITION_NAME "development"
