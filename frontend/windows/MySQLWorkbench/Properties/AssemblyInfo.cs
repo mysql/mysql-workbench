effb5e48375c427b86442464701da270499478cd
Rene Ramirez <j.rene.ramirez@oracle.com>
2026-03-23 18:15:25 -0600

    Upgrading version to 8.0.47

--- frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
+++ frontend/windows/MySQLWorkbench/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using MySQL.GUI.Workbench;
 //      Minor Version 
 //      Release Number
 //
-[assembly: AssemblyVersion("8.0.46")]
-[assembly: AssemblyFileVersion("8.0.46")]
+[assembly: AssemblyVersion("8.0.47")]
+[assembly: AssemblyFileVersion("8.0.47")]
